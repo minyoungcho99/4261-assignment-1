68f480afb7c0c173e8021aef284a5a43de391b60
jsjclink <60886172+jsjclink@users.noreply.github.com>
2024-01-28 04:44:47 -0500

    project init. Hello world

---
+++ assignment1/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+export default App;
